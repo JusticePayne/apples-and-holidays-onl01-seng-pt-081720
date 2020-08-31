685546cf50c19c62133f776b07f2815079534b2f
Justice Payne <spikepayne@yahoo.com>
2020-08-31 01:24:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,13 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  array = []
+  holiday_hash.each. do |season, holiday|
+    holiday.each do |name, supply|
+    if supply.include?("BBQ")
+      array<<name
+    end
+    end
+  end
+  array
 end
