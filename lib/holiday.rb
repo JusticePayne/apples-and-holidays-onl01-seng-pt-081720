b8675acb26a24890910850075501728b2946e08d
Justice Payne <spikepayne@yahoo.com>
2020-08-31 02:04:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supplies|
-      puts "#{event.to_.split("_").map do ([x].capitalize.join(" ") #supplies.join(", "))"
+      puts "#{event.to_.split("_").map do ([x].capitalize.join(" ")) #supplies.join(", "))"
     end
   end
 end
