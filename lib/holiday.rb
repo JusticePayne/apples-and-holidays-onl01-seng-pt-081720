1eddacbef97bdb2ca9574a92575e7c6355cb4705
Justice Payne <spikepayne@yahoo.com>
2020-08-31 02:09:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,31 +1,50 @@
-require 'pry'
-
-def second_supply_for_fourth_of_july(holiday_hash)
-  holiday_hash[:summer][:fourth_of_july][1]
+def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter].each{|key, vaule|holiday_hash[:winter][key]<<supply}
+def add_supply_to_winter_holidays(holiday_hash, item)
+ holiday_hash[:winter].each do |holiday, decorations|
+   decorations << item
+  end
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[:spring][:memorial_day]<<supply
+  holiday_hash[:spring][:memorial_day].push(supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season]={holiday_name=>supply_array}
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
+end
+
+def add_new_season_and_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  holiday_hash[season] = {holiday_name => supply_array}
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
 end
 
-
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    puts "#{season.to_s.capitalize}:"
-    holiday.each do |event, supplies|
-      puts "#{event.to_.split("_").map do ([x].capitalize.join(" "))} #supplies.join(", "))"
-    end
-  end
+  holiday_hash.each do | season, holiday|
+     puts "#{season.capitalize}:"
+     holiday.each do |holiday, item|
+       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+     end
+  end 
 end
+
+all_supplies_in_holidays(holiday_supplies)
+
+
+
+def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, item|
+      holiday if item.include?("BBQ")
+    end 
+  end.flatten.compact
+end
+
+all_holidays_with_bbq(holiday_supplies)
