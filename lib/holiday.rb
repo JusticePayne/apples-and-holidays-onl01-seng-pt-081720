91bb72962f05ea6ad14469a30ec8afa6ea988ab9
Justice Payne <spikepayne@yahoo.com>
2020-08-31 01:44:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,16 +22,12 @@ end
 
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    puts "#{season.to_s.capitalize!}:"
-    holiday.each do |name, supply|
-      new_array = []
-      array=name.to_s.split("array.each{|word|new_array<<word.capitalize!}")
-      name = new_array.join("")
-      supply = supply.join(", ")
-      puts"#{name}:#{supply}"
-      end
-    end
+  holiday_hash.each do | season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, item|
+      puts " #{holiday.to_s.split("_").map {|i| i.capitalize.join(" ")}}": 
+    end  
+ end
 end
 
 def all_holidays_with_bbq(holiday_hash)
